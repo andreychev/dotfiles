393a63b91e855c1cd8a031ad6ed2659ec1c0b10e
Philipp Andreychev <andreychev@me.com>
2023-11-03 11:19:52 +0300

    Migrate base keyboard setup to .config folder

--- tasks/download.sh
+++ tasks/download.sh
@@ -8,6 +8,4 @@ cd $XDG_DOWNLOAD_DIR
 curl -fLo "ilya-birman-typolayout-$TYPOGRAPHY-mac.dmg" "https://ilyabirman.ru/typography-layout/download/ilya-birman-typolayout-$TYPOGRAPHY-mac.dmg"
 curl -fLo "$CHROMIUM_GOST-macos-arm64.tar.bz2" "https://github.com/deemru/Chromium-Gost/releases/download/$CHROMIUM_GOST/chromium-gost-$CHROMIUM_GOST-macos-arm64.tar.bz2"
 curl -fLo "AmneziaVPN_$AMNEZIA.dmg" "https://github.com/amnezia-vpn/amnezia-client/releases/download/${AMNEZIA%.*}/AmneziaVPN_$AMNEZIA.dmg"
-
-curl -fLo NEO65_0816.json https://cdn.shopify.com/s/files/1/0508/8716/4061/files/NEO65_0816.json
 cd $HOME

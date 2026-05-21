01d6fcb11beaf84905b09f2450f61d0669842e5e
Philipp Andreychev <andreychev@me.com>
2026-05-21 10:03:30 +0300

    fix: zsh history settings

--- .config/zsh/environment.zsh
+++ .config/zsh/environment.zsh
@@ -9,3 +9,11 @@ setopt CDABLE_VARS       # Change directory to a path stored in a variable.
 setopt MULTIOS           # Write to multiple descriptors.
 setopt EXTENDED_GLOB     # Use extended globbing syntax.
 
+HISTSIZE=10000          # Max number of history entries in memory.
+SAVEHIST=10000          # Max number of history entries saved to disk.
+
+setopt SHARE_HISTORY         # Share history between all sessions.
+setopt HIST_IGNORE_ALL_DUPS  # Remove older duplicate entries from history.
+setopt HIST_IGNORE_SPACE     # Do not record entries starting with a space.
+setopt HIST_REDUCE_BLANKS    # Remove superfluous blanks from history entries.
+
